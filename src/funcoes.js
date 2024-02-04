1277291ee19530b6c76eeae2ba0d3bf687fcd173
lidice <lidicedupuit@hotmail.com>
2024-02-04 10:30:28 -0300

    exerciciots

---
+++ src/funcoes.js
@@ -0,0 +1,5 @@
+"use strict";
+function calculaNumero(a, b) {
+    return a * b;
+}
+const calculaNumero2 = (a, b) => a * b;
---
+++ src/funcoes.ts
@@ -0,0 +1,4 @@
+function calculaNumero(a: number, b: number): number{
+    return a * b;
+}
+const calculaNumero2 = (a: number, b: number): number => a *b
